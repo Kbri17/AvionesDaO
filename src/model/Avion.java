2772781f690a958b130926d0c32444bfe1fb37ee
Kbri17 <104737155+Kbri17@users.noreply.github.com>
2024-08-12 14:03:00 -0500

    modelo avance

--- src/model/Avion.java
+++ src/model/Avion.java
@@ -1,4 +1,68 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Avion {
+
+    private Integer id;
+    private String marca;
+    private String modelo;
+    private String matricula;
+
+    private LocalDate fechaEntradaServicio;
+
+    public Avion(String marca, String modelo, String matricula, LocalDate fechaEntradaServicio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.fechaEntradaServicio = fechaEntradaServicio;
+    }
+
+    public Avion(Integer id, String marca, String modelo, String matricula, LocalDate fechaEntradaServicio) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.fechaEntradaServicio = fechaEntradaServicio;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public LocalDate getFechaEntradaServicio() {
+        return fechaEntradaServicio;
+    }
+
+    public void setFechaEntradaServicio(LocalDate fechaEntradaServicio) {
+        this.fechaEntradaServicio = fechaEntradaServicio;
+    }
 }
